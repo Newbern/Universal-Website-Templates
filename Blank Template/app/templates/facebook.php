ab06f8073ee1e8b6db5301c15b1e75770d0aec75
steve <StevencNewbern@gmail.com>
2025-12-09 20:37:39 -0600

    12/9/25 Facebook Front-End

--- Blank Template/app/templates/facebook.php
+++ Blank Template/app/templates/facebook.php
@@ -1,11 +1,15 @@
 <div class="main-block">
     <h1>Facebook Integration</h1>
 
-    <div class="facebook">
-        <button>previous</button>
-        <div class="post">
-            <?php  ?>
+    <div class="gallery">
+        <button class='gallery prev'>&#8592;</button>
+        <div class="facebook-slide">
+            <img class="facebook-post prev inactive" src="https://scontent-atl3-3.xx.fbcdn.net/v/t51.82787-15/589933371_18546042460019916_3300413713669042778_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=iwM7uCNlR-IQ7kNvwGgqj5R&_nc_oc=AdnhmUHeUsUEHzpOxZc6SM_4OZ5MB1StFYgHhYKZqFPQ9n2jTBW0gY3ic7IwMP-Rt4JknA2PAfCltQksBqQCiALI&_nc_zt=23&_nc_ht=scontent-atl3-3.xx&_nc_gid=D5v0ulUssIUPAAUyEYnEHg&oh=00_Afl1-bBLdSExfu6q6Jx2cZ_cRqRf_QRe6EH2kwQsAC6ydg&oe=693C100D">
+            <img class="facebook-post inactive" src="https://scontent-atl3-3.xx.fbcdn.net/v/t51.82787-15/589933371_18546042460019916_3300413713669042778_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=iwM7uCNlR-IQ7kNvwGgqj5R&_nc_oc=AdnhmUHeUsUEHzpOxZc6SM_4OZ5MB1StFYgHhYKZqFPQ9n2jTBW0gY3ic7IwMP-Rt4JknA2PAfCltQksBqQCiALI&_nc_zt=23&_nc_ht=scontent-atl3-3.xx&_nc_gid=D5v0ulUssIUPAAUyEYnEHg&oh=00_Afl1-bBLdSExfu6q6Jx2cZ_cRqRf_QRe6EH2kwQsAC6ydg&oe=693C100D">
+            <img class="facebook-post curnt" src="https://scontent-atl3-3.xx.fbcdn.net/v/t51.82787-15/589933371_18546042460019916_3300413713669042778_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=iwM7uCNlR-IQ7kNvwGgqj5R&_nc_oc=AdnhmUHeUsUEHzpOxZc6SM_4OZ5MB1StFYgHhYKZqFPQ9n2jTBW0gY3ic7IwMP-Rt4JknA2PAfCltQksBqQCiALI&_nc_zt=23&_nc_ht=scontent-atl3-3.xx&_nc_gid=D5v0ulUssIUPAAUyEYnEHg&oh=00_Afl1-bBLdSExfu6q6Jx2cZ_cRqRf_QRe6EH2kwQsAC6ydg&oe=693C100D">
+            <img class="facebook-post inactive" src="https://scontent-atl3-3.xx.fbcdn.net/v/t51.82787-15/589933371_18546042460019916_3300413713669042778_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=iwM7uCNlR-IQ7kNvwGgqj5R&_nc_oc=AdnhmUHeUsUEHzpOxZc6SM_4OZ5MB1StFYgHhYKZqFPQ9n2jTBW0gY3ic7IwMP-Rt4JknA2PAfCltQksBqQCiALI&_nc_zt=23&_nc_ht=scontent-atl3-3.xx&_nc_gid=D5v0ulUssIUPAAUyEYnEHg&oh=00_Afl1-bBLdSExfu6q6Jx2cZ_cRqRf_QRe6EH2kwQsAC6ydg&oe=693C100D">
+            <img class="facebook-post next inactive" src="https://scontent-atl3-3.xx.fbcdn.net/v/t51.82787-15/589933371_18546042460019916_3300413713669042778_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=iwM7uCNlR-IQ7kNvwGgqj5R&_nc_oc=AdnhmUHeUsUEHzpOxZc6SM_4OZ5MB1StFYgHhYKZqFPQ9n2jTBW0gY3ic7IwMP-Rt4JknA2PAfCltQksBqQCiALI&_nc_zt=23&_nc_ht=scontent-atl3-3.xx&_nc_gid=D5v0ulUssIUPAAUyEYnEHg&oh=00_Afl1-bBLdSExfu6q6Jx2cZ_cRqRf_QRe6EH2kwQsAC6ydg&oe=693C100D">
         </div>
-        <button>next</button>
+        <button class='gallery next'>&#8594;</button>
     </div>
 </div>
